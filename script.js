bcaa4c9ea5b48310ec6e8a303f0f699f1c21d722
KauMiLima <freekaua@gmail.com>
2025-05-01 12:35:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.getElementById("yes-btn");
 const noBtn = document.getElementById("no-btn");
 
 yesBtn.addEventListener("click", () => {
-    question.innerHTML = "Eu sabia 😍"
+    question.innerHTML = "Pode mandar 98982167252 😍"
     gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
 
